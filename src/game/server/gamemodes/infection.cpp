gwilherm/teeworlds-infection

+Import Eye emote feature from DDnet +Clean a bit Whisper code

src/game/server/gamemodes/infection.cpp
@@ -10,7 +10,7 @@
 
 CGameControllerInfection::CGameControllerInfection(class CGameContext *pGameServer)
 : IGameController(pGameServer) {
-    m_pGameType = "bInfection";
+    m_pGameType = "bInfection+";
 	m_BroadcastTime = 0;
 	m_NextZombie = 0;
 }

gwilherm/teeworlds

src/game/server/gamemodes/infection.cpp
@@ -9,7 +9,7 @@
 
 CGameControllerInfection::CGameControllerInfection(class CGameContext *pGameServer)
 : IGameController(pGameServer) {
-	m_pGameType = "Infection";
+	m_pGameType = "Infection+";
 	m_BroadcastTime = 0;
 	m_NextZombie = 0;
 }